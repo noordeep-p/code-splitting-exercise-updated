c37553f4338e043098cd3b395bed631c2b9bf212
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-05-16 21:34:30 -0400

    import components using react lazy

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
-import React, { Component, Suspense } from 'react';
-import './App.css';
+import React, { Component, Suspense } from "react";
+import "./App.css";
 
-import Page1 from './Components/Page1';
-import Page2 from './Components/Page2';
-import Page3 from './Components/Page3';
+import Page1 from "./Components/Page1";
+// import Page2 from './Components/Page2';
+// import Page3 from './Components/Page3';
+
+const Page2Lazy = React.lazy(() => import("./Components/Page2"));
+const Page3Lazy = React.lazy(() => import("./Components/Page3"));
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      route: 'page1',
-    }
+      route: "page1",
+    };
   }
   onRouteChange = (route) => {
     this.setState({ route: route });
-  }
+  };
   render() {
-    if (this.state.route === 'page1') {
-      return <Page1 onRouteChange={this.onRouteChange} />
-    } else if (this.state.route === 'page2') {
-      return <Page2 onRouteChange={this.onRouteChange} />
+    if (this.state.route === "page1") {
+      return <Page1 onRouteChange={this.onRouteChange} />;
+    } else if (this.state.route === "page2") {
+      return <Page2 onRouteChange={this.onRouteChange} />;
     } else {
-      return <Page3 onRouteChange={this.onRouteChange} />
+      return <Page3 onRouteChange={this.onRouteChange} />;
     }
-
   }
 }
 
